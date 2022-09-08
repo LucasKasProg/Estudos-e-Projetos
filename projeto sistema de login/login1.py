c1163c1407abc8e50aeadd127388a3a28d8fd0d2
Lucas Queiroz <92536070+LucasKasProg@users.noreply.github.com>
2022-09-08 13:44:14 -0300

    Criando mais um projeto

---
+++ projeto sistema de login/login1.py
@@ -0,0 +1,19 @@
+#projeto sistema de login
+
+#dar as primeiras opções ao usuario
+
+print('Bem vindo ao sistema de login!')
+print('Para efetuar cadastro digite 1 ')
+print('Para efetuar o login digite 2')
+print('Para sair digite 3')
+
+acao = int(input(''))
+
+if acao == '1' 
+
+print('acao1')
+
+acao1 = ('Selecione um nome de usuario:')
+usuario = input('')
+
+
