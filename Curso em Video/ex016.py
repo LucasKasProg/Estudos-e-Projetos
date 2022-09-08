4819c177bf5d0324c5eb066e20ab3fdffa88eb6a
Lucas Queiroz <92536070+LucasKasProg@users.noreply.github.com>
2022-09-08 14:23:57 -0300

    exs

---
+++ Curso em Video/ex016.py
@@ -0,0 +1,7 @@
+import math
+
+num = float(input('Digite um numero:'))
+
+math.trunc(num)
+
+print('O numero {} tem a parte inteira de {}'.format(num, math.trunc(num)))
---
+++ Curso em Video/ex019.py
@@ -0,0 +1,9 @@
+import random
+
+print('Sorteio para decidir quem limpara o quadro!')
+p1 = input('Digite o nome do primeiro aluno a partipar do sorteio:')
+p2 = input('Digite o nome do segundo aluno a partipar do sorteio:')
+p3 = input('Digite o nome do terceiro aluno a partipar do sorteio:')
+p4 = input('Digite o nome do quarto aluno a partipar do sorteio:')
+
+random.random(1, 4)
