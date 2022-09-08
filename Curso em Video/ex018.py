184f894696437f0439bc50a4f2ce3f73946374d0
Lucas Queiroz <92536070+LucasKasProg@users.noreply.github.com>
2022-09-08 15:12:58 -0300

    exs

---
+++ Curso em Video/ex018.py
@@ -0,0 +1,16 @@
+import math
+
+an = float(input('Digite o ângulo que você deseja:'))
+
+seno = math.sin(math.radians(an))
+
+cosse = math.cos(math.radians(an))
+
+tang = math.tan(math.radians(an))
+
+print('O ângulo de {} tem o SENO de {:.2f}' .format(an, seno))
+
+print('O ângulo de {} tem o COSSENO de {:.2f}' .format(an, cosse))
+
+print('O ânguo de {} tem o TANGENTE de {:.2f}'.format(an, tang))
+
--- Curso em Video/ex019.py
+++ Curso em Video/ex019.py
@@ -6,4 +6,8 @@ p2 = input('Digite o nome do segundo aluno a partipar do sorteio:')
 p3 = input('Digite o nome do terceiro aluno a partipar do sorteio:')
 p4 = input('Digite o nome do quarto aluno a partipar do sorteio:')
 
-random.random(1, 4)
+lista = [p1, p2, p3, p4]
+
+escolhido = random.choice(lista)
+
+print('O escolhido para limpar o quadro foi {}!'.format(escolhido))
